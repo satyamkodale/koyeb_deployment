4f258d515a8f5f675934eef8502fd1434ee845a7
satyamkodale <satyamkodale@gmail.com>
2024-09-19 07:13:35 +0530

    added say hello endpoint

--- src/main/java/com/h2db/H2DBExample/Controllers/UserController.java
+++ src/main/java/com/h2db/H2DBExample/Controllers/UserController.java
@@ -3,6 +3,8 @@ package com.h2db.H2DBExample.Controllers;
 import com.h2db.H2DBExample.Entities.User;
 import com.h2db.H2DBExample.Repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,4 +26,11 @@ public class UserController {
  
         return userRepository.save(user);
     }
+    
+    @GetMapping("/say hello")
+    public ResponseEntity<String>  sayHello()
+    {
+    	return new ResponseEntity("hello",HttpStatus.OK);
+    }
+    
 }
